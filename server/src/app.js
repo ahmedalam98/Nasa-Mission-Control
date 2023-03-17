d72fd6949d027654b51c112727989ba249c2a731
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-03-17 08:36:00 +0200

    feat: :sparkles: Load planets and return as JSON.
    Allow CORS to accept getPlanets request from the front-end PORT

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,25 @@
 // API server setup
 const express = require("express");
-
 const app = express();
+
+const cors = require("cors");
+
+const planetsRouter = require("./routes/planets/planets.router");
+
+///////////////////
+/* MiddleWares */
+///////////////////
+
+// Allow CORS to accept requests from port 3000 ( front-end )
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 // Using express middleware to parse incoming JSON from incoming requests body
 app.use(express.json());
+
+app.use(planetsRouter);
 
 module.exports = app;
