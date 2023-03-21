7fdc002a1a9d1b65bc3f05fa5a623b44f01df507
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-03-21 17:24:59 +0200

    feat: :sparkles: Start upsert operation

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,10 +1,11 @@
 const fs = require("fs");
 const path = require("path");
 const { parse } = require("csv-parse");
+const planets = require("./planets.mongo");
 
-const habitablePlaents = [];
+// const habitablePlaents = [];
 
-function isHabitablePlaent(planet) {
+function isHabitablePlanet(planet) {
   return (
     planet["koi_disposition"] === "CONFIRMED" &&
     planet["koi_insol"] > 0.36 &&
@@ -24,24 +25,49 @@ function loadPlanetsData() {
           columns: true,
         })
       )
-      .on("data", (data) => {
-        if (isHabitablePlaent(data)) {
-          habitablePlaents.push(data);
+      .on("data", async (data) => {
+        if (isHabitablePlanet(data)) {
+          savePlanet(data);
         }
       })
       .on("error", (err) => {
         console.log(err);
         reject(err);
       })
-      .on("end", () => {
-        console.log(`${habitablePlaents.length} habitable planets found`);
+      .on("end", async () => {
+        const countPlanetsFound = (await getAllPlanets()).length;
+        console.log(`found ${countPlanetsFound} planets that are habitable`);
+        console.log("done processing files");
+
         resolve();
       });
   });
 }
 
-function getAllPlanets() {
-  return habitablePlaents;
+async function getAllPlanets() {
+  // find all planets --> {}
+  return await planets.find({});
+}
+
+async function savePlanet(planet) {
+  try {
+    // soving data to mongo instead of array with upsert (insert + update)
+    await planets.updateOne(
+      {
+        // the filter
+        keplerName: planet.kepler_name,
+      },
+      {
+        // the data to insert matching the planets-schema
+        keplerName: planet.kepler_name,
+      },
+      {
+        upsert: true,
+      }
+    );
+  } catch (err) {
+    console.error(`can't save planet ${err}`);
+  }
 }
 
 module.exports = {
